c765570cd1828685045ab2adc5f38118ba3ad969
ainuttaqimzawait <ainuttaqimzawaid1000@gmail.com>
2025-03-17 04:58:02 +0700

    merubah metode config

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-});
+// const sequelize = new Sequelize({
+//     host: process.env.DB_HOST,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+// });
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT, // Gantilah sesuai database yang digunakan ('mysql', 'postgres', 'sqlite', dll.)
+        logging: false, // Opsional: mematikan logging query SQL
+    }
+);
+
 
 // (async () => {
 //     try {
