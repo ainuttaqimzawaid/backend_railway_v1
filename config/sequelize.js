744024c9f45537be52b63b775790f521ff2cb387
ainuttaqimzawait <ainuttaqimzawaid1000@gmail.com>
2025-10-04 08:42:52 +0700

    m

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,27 +1,37 @@
+require("dotenv").config();
+const { Sequelize } = require('sequelize');
+//deklarasi mysql2 agar bisa digunakan divercel
+require('mysql2');
+
+// const sequelize = new Sequelize({
+//     host: process.env.DB_HOST,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+// });
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT, 10),
+        port: process.env.DB_PORT,
         dialect: 'mysql',
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
+        logging: false, // Opsional: mematikan logging query SQL
     }
 );
+
 
 (async () => {
     try {
         await sequelize.authenticate();
-        console.log('✅ Database connected successfully');
+        console.log('Connection has been established successfully.');
     } catch (error) {
-        console.error('❌ DB Connection Error:', error.message);
-        console.error(error); // supaya detail error muncul di Vercel logs
+        console.error('Unable to connect to the database:', error);
     }
-})();
+})()
+
+module.exports = sequelize;
