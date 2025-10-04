0892507aa84cb4f3357f40df63994e0b0a9974b4
ainuttaqimzawait <ainuttaqimzawaid1000@gmail.com>
2025-10-05 00:59:50 +0700

    m

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,37 +3,26 @@ const { Sequelize } = require('sequelize');
 //deklarasi mysql2 agar bisa digunakan divercel
 require('mysql2');
 
-let sequelize;
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        logging: false, // Opsional: mematikan logging query SQL
+    }
+);
 
-if (!global._sequelize) {
-    global._sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            dialect: 'mysql',
-            dialectModule: require('mysql2'),
-            logging: false,
-            pool: {
-                max: 5,
-                min: 0,
-                idle: 10000,
-            },
-        }
-    );
-}
-
-sequelize = global._sequelize;
 
 (async () => {
     try {
         await sequelize.authenticate();
-        console.log('✅ Database connected successfully.');
+        console.log('Connection has been established successfully.');
     } catch (error) {
-        console.error('❌ Unable to connect to the database:', error.message);
+        console.error('Unable to connect to the database:', error);
     }
-})();
+})()
 
 module.exports = sequelize;
