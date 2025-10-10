d5b8c459aa12f51fd9978ce21b19dd099d0c0fdd
ainuttaqimzawait <ainuttaqimzawaid1000@gmail.com>
2025-10-10 20:53:47 +0700

    m;

--- app/product_v2/controller.js
+++ app/product_v2/controller.js
@@ -277,6 +277,7 @@ const destroy = async (req, res, next) => {
       // })
       return res.json({ book, message: 'Book successfully deleted' });
    } catch (err) {
+      console.error(err);
       next(err);
    }
 };
